74228f46ca3d366a261978bd39fceafa8da18cde
SerhiiPodaiko <podajko1997@gmail.com>
2023-11-15 16:21:00 +0200

    Fix useAuth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/navigation'
 import { SubmitHandler } from 'react-hook-form'
 
 import { useAuthStore } from '@store/auth/useAuthStore'
 import { IRegisterUser as IFormRegisterInput, ILoginUser as IFormLoginInput } from '@store/auth/models'
+import { PAGE_SLUGS } from '@constants/pages'
 
 export const useAuth = () => {
+  const router = useRouter()
   const authStore = useAuthStore()
 
-  const onLoginSubmit: SubmitHandler<IFormLoginInput> = data => authStore.setLogin(data)
+  const onLoginSubmit: SubmitHandler<IFormLoginInput> = data => {
+    authStore.setLogin(data)
+    router.push(PAGE_SLUGS.home)
+  }
 
-  const onRegisterSubmit: SubmitHandler<IFormRegisterInput> = data => authStore.setRegister(data)
+  const onRegisterSubmit: SubmitHandler<IFormRegisterInput> = data => {
+    authStore.setRegister(data)
+    router.push(PAGE_SLUGS.home)
+  }
 
   return {
     onLoginSubmit,
